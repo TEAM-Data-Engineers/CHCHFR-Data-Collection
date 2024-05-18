1ffb280c3f5b96b672740189463930b8cf93dec1
Hua Wang <hwa205@uclive.ac.nz>
2024-05-18 21:32:21 +1200

    update z energy api

--- dags/dag_collection_data_from_z.py
+++ dags/dag_collection_data_from_z.py
@@ -1,95 +1,83 @@
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from airflow.hooks.postgres_hook import PostgresHook
 from datetime import datetime
 import requests
-from bs4 import BeautifulSoup
-import json
 import logging
 from entities.gas_station_schema import GasStation
 
+# Database connection parameters
+PG_DATABASE = Variable.get("PG_DATABASE", default_var="p_database")
+PG_USER = Variable.get("PG_USER", default_var="root")
+PG_PASSWORD = Variable.get("PG_PASSWORD", default_var="password")
+PG_HOST = Variable.get("PG_HOST", default_var="host.docker.internal")
+PG_PORT = Variable.get("PG_PORT", default_var="5433")
+
+# Z Energy API parameters
+API_KEY = Variable.get("google_api_key")
+LOCATION = "-43.5252283,172.6375393"
+RADIUS = 5000000
+KEYWORD = "Z Energy"
+TYPES = "gas_station"
+
+# Default arguments for the DAG
 default_args = {
-    'owner': 'Aemon',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 4, 12),
-    'retries': 5,
+    "owner": "Aemon",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 4, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 5
 }
 
+# Define the DAG
 dag = DAG(
-    'collection_data_from_z_v3',
+    "collection_data_from_z_energy_v1",
     default_args=default_args,
-    description='A DAG to collect Z Energy station data and insert into PostgreSQL',
-    schedule_interval='0 1 * * *',  # Daily at 1 AM
+    description="Fetch and insert Z Energy gas stations into PostgreSQL database",
+    schedule_interval="@daily",
     catchup=False
 )
 
-wait_for_table_creation = ExternalTaskSensor(
-    task_id='wait_for_table_creation',
-    external_dag_id='create_gas_station_table',
-    external_task_id='create_gas_station_table',
-    timeout=600,
-    poke_interval=30,
-    mode='poke',
-    dag=dag,
-)
-
-def get_station_data():
-    url = "https://www.z.co.nz/find-a-station"
+def fetch_gas_stations():
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={API_KEY}&location={LOCATION}&radius={RADIUS}&keyword={KEYWORD}&types={TYPES}"
     try:
-        logging.info(f"Fetching data from URL: {url}")
         response = requests.get(url)
         response.raise_for_status()
-        html_content = response.content
-        logging.info("Successfully fetched HTML content")
-        soup = BeautifulSoup(html_content, "html.parser")
-        div_locator = soup.find("div", class_="locator")
-        if div_locator:
-            div_data_props = div_locator.find("div", attrs={"data-props": True})
-            if div_data_props:
-                data_props_json = json.loads(div_data_props["data-props"])
-                stations_data = data_props_json.get("stations", [])
-                logging.info("Successfully fetched and parsed station data")
-                return stations_data
-            else:
-                logging.error("Cannot find data-props attribute in div element")
-                raise Exception("Cannot find data-props attribute in div element")
-        else:
-            logging.error("Cannot find locator class in div element")
-            raise Exception("Cannot find locator class in div element")
+        stations = response.json().get("results", [])
+        logging.info(f"The number of Z Energy gas stations found: {len(stations)}")
+        return stations
     except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching station data: {e}")
-        raise
-    except Exception as e:
-        logging.error(f"Error parsing station data: {e}")
-        raise
+        logging.error(f"Error fetching gas stations: {e}")
+        return []
 
-def check_and_insert_data():
-    logging.info("Starting check_and_insert_data task")
-    stations_data = get_station_data()
+def get_value_after_last_comma(string):
+    if "," in string:
+        return string.split(",")[-1].strip()
+    return ""
 
+def fetch_and_insert_gas_stations(**kwargs):
     hook = PostgresHook(postgres_conn_id='postgres_default')
     conn = hook.get_conn()
     cur = conn.cursor()
 
     try:
-        for station_data in stations_data:
-            location_id = station_data.get('externalID')
-            logging.info(f"Processing station: {location_id}")
+        for station in fetch_gas_stations():
+            location_id = station.get("place_id")
             cur.execute("SELECT COUNT(*) FROM gas_station WHERE location_id = %s", (location_id,))
             count = cur.fetchone()[0]
             if count == 0:
                 gas_station = GasStation(
                     location_id=location_id,
-                    brand_name="Z Energy",
-                    location_name=station_data.get("name"),
-                    latitude=station_data.get("latitude"),
-                    longitude=station_data.get("longitude"),
-                    address_line1=station_data.get("address"),
-                    city=station_data.get("city"),
-                    state_province=station_data.get("region"),
-                    postal_code=station_data.get("postcode"),
+                    brand_name=station.get("name"),
+                    location_name=station.get("name"),
+                    latitude=station.get("geometry", {}).get("location", {}).get("lat"),
+                    longitude=station.get("geometry", {}).get("location", {}).get("lng"),
+                    address_line1=station.get("vicinity"),
+                    city=get_value_after_last_comma(station.get("vicinity")),
+                    state_province="",
+                    postal_code="",
                     country="NZ"
                 )
                 cur.execute("""
@@ -106,7 +94,6 @@ def check_and_insert_data():
             else:
                 logging.info(f"Data already exists for location: {location_id}")
         conn.commit()
-        logging.info("Data insertion completed successfully")
     except Exception as e:
         logging.error(f"Error inserting data: {e}")
         conn.rollback()
@@ -117,11 +104,10 @@ def check_and_insert_data():
         logging.info("Database connection closed")
 
 # Define the tasks
-task_check_and_insert_data = PythonOperator(
-    task_id='check_and_insert_data',
-    python_callable=check_and_insert_data,
-    dag=dag,
+task_fetch_and_insert_gas_stations = PythonOperator(
+    task_id="fetch_and_insert_gas_stations",
+    python_callable=fetch_and_insert_gas_stations,
+    dag=dag
 )
 
-# Define task dependencies
-wait_for_table_creation >> task_check_and_insert_data
+task_fetch_and_insert_gas_stations
